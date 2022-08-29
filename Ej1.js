ea835bc1566a63fa92a7ce7ca649cf0188bb81ef
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-08-29 10:56:52 -0300

    Ej 1 y 2 resueltos

--- Ej1.js
+++ Ej1.js
@@ -1,76 +1,77 @@
-const array = [
+const alumnos = [
 
-{
-    nombre: "Juan",
-    apellido: "Gomez",
-    dni: "46345678",
-    anio: 4,
-    curso: "A",
-    nota: 8,
-},
+    //objetos no tienen nombre
+    {
+        nombre: "Juan",
+        apellido: "Gomez",
+        dni: "46345678",
+        anio: 4,
+        curso: "A",
+        nota: 8,
+    },
 
-{
-    nombre: "Juan",
-    apellido: "Perez",
-    dni: "46349576",
-    anio: 4,
-    curso: "A",
-    nota: 9,
-},
+    {
+        nombre: "Juan",
+        apellido: "Perez",
+        dni: "46349576",
+        anio: 4,
+        curso: "A",
+        nota: 9,
+    },
 
-{
-    nombre: "Ambar",
-    apellido: "Jaichenko",
-    dni: "46486024",
-    anio: 5,
-    curso: "A",
-    nota: 10,
-},
+    {
+        nombre: "Ambar",
+        apellido: "Jaichenko",
+        dni: "46486024",
+        anio: 5,
+        curso: "A",
+        nota: 10,
+    },
 
-{
-    nombre: "Matias",
-    apellido: "Fridman",
-    dni: "47185035",
-    anio: 5,
-    curso: "A",
-    nota: 4,
-},
+    {
+        nombre: "Matias",
+        apellido: "Fridman",
+        dni: "47185035",
+        anio: 5,
+        curso: "A",
+        nota: 4,
+    },
 
-{
-    nombre: "German",
-    apellido: "Prokopiec",
-    dni: "47194769",
-    anio: 4,
-    curso: "B",
-    nota: 3,
-},
+    {
+        nombre: "German",
+        apellido: "Prokopiec",
+        dni: "47194769",
+        anio: 4,
+        curso: "B",
+        nota: 3,
+    },
 
-{
-    nombre: "Caro",
-    apellido: "Orbuch",
-    dni: "47936295",
-    anio: 4,
-    curso: "B",
-    nota: 1,
-},
+    {
+        nombre: "Caro",
+        apellido: "Orbuch",
+        dni: "47936295",
+        anio: 4,
+        curso: "B",
+        nota: 1,
+    },
 
-{
-    nombre: "Esteban",
-    apellido: "Roriguez",
-    dni: "45194576",
-    anio: 3,
-    curso: "B",
-    nota: 5,
-},
+    {
+        nombre: "Esteban",
+        apellido: "Roriguez",
+        dni: "45194576",
+        anio: 3,
+        curso: "B",
+        nota: 5,
+    },
 
-{
-    nombre: "Ulises",
-    apellido: "Vazquez",
-    dni: "45195275",
-    anio: 3,
-    curso: "B",
-    nota: 7,
-},
+    {
+        nombre: "Ulises",
+        apellido: "Vazquez",
+        dni: "45195275",
+        anio: 3,
+        curso: "B",
+        nota: 7,
+    },
 
 ];
 
---
+++ Ej2.js
@@ -0,0 +1,5 @@
+const capitalizar = (s) => {
+    
+    return s[0].toUpperCase() + s.slice(1);
+    
+}
